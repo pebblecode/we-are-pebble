59fcd8389837f0c72b985545ee4ba9b8a2d9e8b4
Peter Tait <contact@petertait.com>
2014-07-21 17:02:45 +0100

    Addition to fit in Parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
-(function () { 'use strict';
-
-
-}()); // end 'use strict'
+$(document).ready(function(){
+    
+	// Parallax
+	var s = skrollr.init({
+		edgeStrategy: 'set',
+		easing: {
+			WTF: Math.random,
+			inverted: function(p) {
+				return 1-p;
+			}
+		}
+	});
+	
+});
