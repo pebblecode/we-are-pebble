83c5db7d9d74f49aad228a08d8f8d29da32682e8
Peter Tait <contact@petertait.com>
2014-07-22 17:57:44 +0100

    Added map carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
     
-	// Parallax - Skrollr
-	var s = skrollr.init({
-		edgeStrategy: 'set',
-		easing: {
-			WTF: Math.random,
-			inverted: function(p) {
-				return 1-p;
+	// Parallax - Skrollr (destroy on mobile)
+	if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+		var s = skrollr.init({
+			edgeStrategy: 'set',
+			easing: {
+				WTF: Math.random,
+				inverted: function(p) {
+					return 1-p;
+				}
 			}
-		}
+		});
+	}
+
+	// Map Carousel
+	$('.map-carousel').carousel({ 
+		overflow: true, 
+		visible: 2, 
+		itemMinWidth: 200, 
+		itemMargin: 10 
 	});
 	
 });
