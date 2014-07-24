078158212d8a1fc992e70ada0458fa3bdf213ec7
Peter Tait <contact@petertait.com>
2014-07-24 13:37:00 +0100

    Added map carousel

--- src/js/main.js
+++ src/js/main.js
@@ -14,11 +14,28 @@ $(document).ready(function(){
 	}
 
 	// Map Carousel
-	$('.map-carousel').carousel({ 
-		overflow: true, 
-		visible: 2, 
-		itemMinWidth: 200, 
-		itemMargin: 10 
-	});
+	$(".map-carousel").tinycarousel({
+        axis   : "y",
+        animationTime: 500
+    });
 	
 });
+
+window.onload = function() {
+    var mapOptions = {
+      center: new google.maps.LatLng(51.485672, -0.118554),
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    };
+    var map = new google.maps.Map(document.getElementById("london-map"),
+      mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(51.485672, -0.118554),
+      map: map,
+      title: "Hello World!"
+    });
+
+    $( '.site-footer' ).hide();
+  }
